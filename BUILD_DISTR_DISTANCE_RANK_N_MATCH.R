3198e51f8fac4acb292a2c2f2474b47c0826e146
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2018-02-07 06:38:52 -0600

    need to test code

--- BUILD_DISTR_DISTANCE_RANK_N_MATCH.R
+++ BUILD_DISTR_DISTANCE_RANK_N_MATCH.R
@@ -6,10 +6,10 @@ library(tidyverse)
 
 df <- datasets::airquality # needs to have stores/unit as rownames
 
-# Convert column #1 to rownames and scale the dataset
+# Prep for Distance: Convert column #1 to rownames and scale the dataset
 
 rownames(df) <- df[1]
-df_scaled <- scale(df)
+df_scaled <- scale(df[-1])
 
 #---- Build the Distant Matrix----
 DF_DIST <- dist(df_scaled , method = "euclidian")
@@ -18,14 +18,14 @@ DF_DIST <- dist(df_scaled , method = "euclidian")
 DF_RANK_BASE <- as.matrix(DF_DIST)
 
 # Force NA on upper triangle and diagnol of 0's
-DF_RANK_BASE[upper.tri(DF_RANK_BASE)] <- NA 
+DF_RANK_BASE[upper.tri(DF_RANK_BASE)] <- NA
 diag(DF_RANK_BASE) <- NA
 
-
+#----Produce list of one to one distance Metric----
 DF_RANK_BASE_1 <- reshape2::melt(DF_RANK_BASE)
 names(DF_RANK_BASE_1) <- c("CONTROL","TEST","DIST_Q")
 
-DF_RANK_FINAL <- DF_RANK_BASE_1 %>% na.omit() %>%
+DF_DIST__FINAL <- DF_RANK_BASE_1 %>% na.omit() %>%
                 dplyr::arrange(TEST,DIST_Q,CONTROL)
 
 
