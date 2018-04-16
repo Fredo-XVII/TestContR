e2e42e221a91e6e8eeb7228364ba3f701f43c31c
Alfredo.G.Marquez <Alfredo.G.Marquez@target.com>
2018-04-16 13:00:06 -0500

    building the depuing script

--- Dedupe_Development_Script.R
+++ Dedupe_Development_Script.R
@@ -1,9 +1,51 @@
 # Dev script to remove dupes in Control List
 
+library(tidyverse)
+library(dplyr)
+library(random)
+
+n <- 10
+
 DUPES_LIST <- CONTROL_STR_LIST %>% dplyr::group_by(CONTROL) %>%
               summarise(control_cnt = n()) %>%
               filter(control_cnt > 1)
 
+while (nrow(DUPES_LIST) > 0) {
+
+  # rank the duplicate control stores and filter out the minimum rank
+
+  rank_dupes <- DUPES_LIST %>% inner_join(CONTROL_STR_LIST) %>%
+    group_by(CONTROL) %>% mutate(rank = min_rank(DIST_Q)) %>%
+    filter(rank > 1)
+
+  # Remove the duplicate from CONTROL_STR_LIST distance list
+
+  CONTROL_STR_LIST_TEMP <-
+    CONTROL_STR_LIST %>% left_join(rank_dupes)
+
+  CONTROL_STR_LIST_TEMP <- CONTROL_STR_LIST_TEMP %>%
+    mutate(CONTROL = if_else(is.na(rank) == TRUE, CONTROL, NULL),
+           DIST_Q = if_else(is.na(rank) == TRUE, DIST_Q,NULL))
+
+
+  CONTROL_STR_LIST_TEMP$CONTROL <- ifelse(!is.na(CONTROL_STR_LIST_TEMP$rank), '', CONTROL_STR_LIST_TEMP$CONTROL)
+  CONTROL_STR_LIST_TEMP$DIST_Q <- ifelse(!is.na(CONTROL_STR_LIST_TEMP$rank), '', CONTROL_STR_LIST_TEMP$DIST_Q)
+
+  # Remove the duplicate from remaining distance list
+
+  CONTROL_STR_TOP5_TEMP <- CONTROL_STR_TOP5 %>% anti_join(rank_dupes, by = "CONTROL")
+
+  # select new minimum from the remaining list
+
+  # Add new control to test stores with missing controls stores
+
+  # re-build the Dupes_list
+
+  # end of dupe list is nrow() = 0
+
+
+
+}
 
 for (i in rnow(CONTROL_STR_LIST)) {
   if (nrow(DUPES_LIST) == 0) {
