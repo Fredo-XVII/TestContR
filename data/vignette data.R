102820960300a447d5f51d21a1ac7e7265dc27c8
Alfredo.G.Marquez <Alfredo.G.Marquez@target.com>
2018-04-18 14:47:59 -0500

    code is ready for production

--- data/vignette data.R
+++ data/vignette data.R
@@ -1,12 +1,13 @@
 
+# library(tidyverse)
 #df <- datasets::iris # needs to have stores/unit as rownames
 #df$flower_num <- as.integer(rownames(df))
 #df <- select(df, flower_num, everything()) %>% select(-Species)
 
-df <- USArrests %>% mutate(state = row.names(USArrests)) %>%
-      select(state, everything())
-df <- readRDS('data/sales.rds')
-
-list_test <- tribble(~TEST, 1102, 2605, 847, 1375 )
-list_test <- tribble(~TEST )
+# df <- USArrests %>% mutate(state = row.names(USArrests)) %>%
+#       select(state, everything())
+# df <- readRDS('data/sales.rds')
+#
+# list_test <- tribble(~TEST, 1102, 2605, 847, 1375 )
+# list_test <- tribble(~TEST )
 
