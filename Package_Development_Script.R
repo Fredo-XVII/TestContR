c8c3ed49e392b9e44a0de0d8374fbe880669fd4a
Alfredo.G.Marquez <Alfredo.G.Marquez@target.com>
2018-04-20 10:34:09 -0500

    making some minor changes

--- Package_Development_Script.R
+++ Package_Development_Script.R
@@ -10,7 +10,6 @@ library(roxygen2)
 
 use_mit_license("Alfredo G Marquez")
 pckg_list <- c("random","tidyverse","reshape2")
-use_package( "random", type = "Import")
 use_package( "tidyverse", type = "Import")
 use_package( "reshape2", type = "Import")
 
